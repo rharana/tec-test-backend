f933f782656f751783cd8f62cf7963971205c53e
rharana <rharana2188@gmail.com>
2025-06-01 17:41:44 +0200

    Initial commit

---
+++ src/main/java/com/test/api/marvel_challenge/MarvelChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.test.api.marvel_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/api/marvel_challenge/MarvelChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.api.marvel_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
