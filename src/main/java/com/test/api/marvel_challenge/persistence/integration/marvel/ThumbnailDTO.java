7dc636e041b3f0143db296d3cfaff0e9c54836b6
rharana <rharana2188@gmail.com>
2025-06-01 19:44:47 +0200

    fix: Folder Structure Redo

--- src/main/java/com/test/api/marvel_challenge/persistence/integration/marvel/ThumbnailDTO.java
+++
@@ -1,4 +0,0 @@
-package com.test.api.marvel_challenge.persistence.integration.marvel;
-
-public record ThumbnailDTO() {
-}
--- src/main/java/com/test/api/marvel_challenge/persistence/integration/marvel/dto/ComicDTO.java
+++ src/main/java/com/test/api/marvel_challenge/persistence/integration/marvel/dto/ComicDTO.java
@@ -1,4 +1,4 @@
-package com.test.api.marvel_challenge.persistence.integration.marvel;
+package com.test.api.marvel_challenge.persistence.integration.marvel.dto;
 
 public record ComicDTO(
         Long id,
